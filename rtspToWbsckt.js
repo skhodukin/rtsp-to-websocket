2c64e0e97e891863bfd22fadd8386e43e4425230
skhodukin <sergii.khodukin@altexsoft.com>
2022-06-03 14:50:03 +0300

    test

---
+++ rtspToWbsckt.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const { proxy, scriptUrl } = require('rtsp-relay')(app);
+
+const handler = proxy({
+  url: `rtsp://demo:demo@ipvmdemo.dyndns.org:5541/onvif-media/media.amp?profile=profile_1_h264&sessiontimeout=60&streamtype=unicast`,
+  // if your RTSP stream need credentials, include them in the URL as above
+  verbose: false,
+});
+
+// the endpoint our RTSP uses
+app.ws('/api/stream', handler);
+
+// this is an example html page to view the stream
+app.get('/', (req, res) =>
+  res.send(`
+  <canvas id='canvas'></canvas>
+
+  <script src='${scriptUrl}'></script>
+  <script>
+    loadPlayer({
+      url: 'ws://' + location.host + '/api/stream',
+      canvas: document.getElementById('canvas')
+    });
+  </script>
+`),
+);
+
+app.listen(2000);
