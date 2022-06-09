af974929323df74afcf0d3ccf28dceddb910ef49
skhodukin <sergii.khodukin@altexsoft.com>
2022-06-09 12:48:41 +0300

    one more step with service commands

--- rtsp-server-relay.js
+++ rtsp-server-relay.js
@@ -1,46 +1,53 @@
 const WebSocket = require('ws');
-
 const socket = new WebSocket('ws://localhost:2000');
-
 /** * */
 const rtsp = require('rtsp-server');
-
 const server = rtsp.createServer((req, res) => {
   console.log(req.method, req.url);
-
   switch (req.method) {
     case 'OPTIONS':
       res.setHeader('Public', 'OPTIONS');
+      res.end();
       break;
     case 'PLAY':
       res.setHeader('Public', 'PLAY');
+      socket.onmessage = function (event) {
+        console.log(event.data);
+        res.write(event.data);
+      }
       break;
     case 'SETUP':
       res.setHeader('Public', 'SETUP');
+      res.end();
       break;
     case 'TEARDOWN':
       res.setHeader('Public', 'TEARDOWN');
       break;
     case 'DESCRIBE':
-      res.setHeader('Public', 'DESCRIBE');
+      res.setHeader('Content-Base', 'rtsp://127.0.0.1:2001');
+      res.setHeader('Content-Type', 'application/sdp');
+      res.end();
       break;
     case 'PAUSE':
       res.setHeader('Public', 'PAUSE');
       break;
-    default:
-      res.statusCode = 501; // Not implemented
   }
-  socket.addEventListener('open', (e) => {
-    socket.on('message', (m) => {
-      // console.log('m', m)
-      res.end(m);
-    });
-    // send a message to the server
 
-    console.log('open');
-  });
+
+
+  // socket.addEventListener('open', (e) => {
+  //   socket.on('message', (m) => {
+  //     console.log('m', m)
+  //
+  //     res.end(m);
+  //   });
+  //   // send a message to the server
+  //
+  //   console.log('open');
+  // });
   // will echo the CSeq header used in the request
 });
+
 
 server.listen(2001, () => {
   const { port } = server.address();
